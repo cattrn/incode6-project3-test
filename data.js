44633337ae99303b084fd1dee79dc76aef5aeb55
Caterina Turnbull <caterina.turnbull@gmail.com>
2021-10-12 10:52:27 +1100

    Initialise npm and alter data to reflect posts instead of schedules

---
+++ data.js
@@ -0,0 +1,38 @@
+module.exports.users = [
+    {
+        "firstname": "James",
+        "lastname": "Bond",
+        "email": "james.bond@gmail.com",
+        "password": "b6b7fb4cad4bc020f76e16889a8e9065cb708d0f8c304a8a3db609b644da9536"  
+    },
+    {
+        "firstname": "Tony",
+        "lastname": "Stark",
+        "email": "starkrulz@gmail.com",
+        "password": "a836ebba36776b21dd0f5cdca497bff65c5bdfc8411cfbfe0111f27bde1c1894"  
+    },
+    {
+        "firstname": "Ali",
+        "lastname": "G",
+        "email": "nameisnotborat@gmail.com",
+        "password": "3b5fe14857124335bb8832cc602f8edcfa12db42be36b135bef5bca47e3f2b9c"  
+    }
+]
+
+module.exports.posts = [
+    {
+        "user_id": 0,
+        "title": "My first post",
+        "body": "Hi there, this is my first post, I don't really know what to write about."
+    },
+    {
+        "user_id": 0,
+        "title": "Breakfast",
+        "body": "Had leftover pizza this morning, yum!"
+    },
+    {
+        "user_id": 2,
+        "title": "Hi mum",
+        "body": "How are you going? I've found this new posting site, hope I'm using it right."
+    }
+]
